34a4d8407894fa61f66908841544d19c8f29c525
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-26 02:49:33 +0700

    Create UseContext

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+import { FavoritesContextProvider } from "./store/favorite-context";
+
 ReactDOM.render(
+  <FavoritesContextProvider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </FavoritesContextProvider>,
   document.getElementById("root")
 );
