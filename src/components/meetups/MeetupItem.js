8e8023c38264db7bc790965c872b0e5a64447e15
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-25 23:15:17 +0700

    Create Card

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,10 @@
+import Card from '../ui/Card'
 import classes from './MeetupItem.module.css';
 
 function MeetupItem (props) {
-    return <li className={classes.item}>
+    return ( 
+    <li className={classes.item}>
+        <Card>
         <div className={classes.image}>
             <img src={props.image} alt={props.title} />
         </div>
@@ -13,7 +16,9 @@ function MeetupItem (props) {
         <div className={classes.actions}>
             <button>To Favorites</button>
         </div>
+        </Card>
     </li>
+    )
 }
 
 export default MeetupItem;
