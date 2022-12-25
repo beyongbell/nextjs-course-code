1285abbb3e23024fa9ab64142ed9085917798053
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-26 01:07:22 +0700

    Post Success

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const navigate = useNavigate();
+
     function addMeetupHandler(meetupData) {
       fetch('https://react-b6358-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json', {
         method: 'POST',
@@ -8,6 +12,8 @@ function NewMeetupPage() {
         headers: {
           'Content-Type': 'application/json'
         }
+      }).then(() => {
+          navigate('/');
       });
     }
 
